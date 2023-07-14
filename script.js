4396b16bb09fcb3e860ad6e2d3ec64d30bf30738
StringManolo <56775178+StringManolo@users.noreply.github.com>
2023-07-14 11:53:22 +0200

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,14 @@ document.addEventListener("DOMContentLoaded", function() {
     var text = paragraph.textContent;
     
     if (text.includes("miFoto_729272927282729172917291919372919172728191928372891.jpg")) {
-      paragraph.classList.add("long-filename");
+      var filename = "miFoto_729272927282729172917291919372919172728191928372891.jpg";
+      
+      var span = document.createElement("span");
+      span.textContent = filename;
+      span.classList.add("long-filename");
+      
+      paragraph.innerHTML = paragraph.innerHTML.replace(filename, span.outerHTML);
+      
       break;
     }
   }
